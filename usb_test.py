e6b3469ad49fb1ddf8cc081df36c7d6f0dd5ce88
Andrei Kniazev <andrei.kniazev@yorlabs.com>
2016-05-26 08:07:53 -0700

    Initial commit.  Basic USB device enumeration works.

---
+++ usb_test.py
@@ -0,0 +1,30 @@
+import usb.core
+import usb.util
+
+# find our device
+dev = usb.core.find(idVendor=0xfffe, idProduct=0x0001)
+
+# was it found?
+if dev is None:
+    raise ValueError('Device not found')
+
+# set the active configuration. With no arguments, the first
+# configuration will be the active one
+dev.set_configuration()
+
+# get an endpoint instance
+cfg = dev.get_active_configuration()
+interface = cfg[(0,0)]
+
+endpoint = usb.util.find_descriptor(
+    interface,
+    # match the first OUT endpoint
+    custom_match = \
+    lambda e: \
+        usb.util.endpoint_direction(e.bEndpointAddress) == \
+        usb.util.ENDPOINT_OUT)
+
+assert endpoint is not None
+
+# write the data
+endpoint.write('test')
