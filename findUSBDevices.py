87f4ff9d80dd7dfc9df62d2ad58de9aca50b844f
Andrei Kniazev <andrei.kniazev@yorlabs.com>
2016-06-01 18:48:58 -0700

    add more examples, USB Device View for win

--- findUSBDevices.py
+++ findUSBDevices.py
@@ -3,6 +3,16 @@ import sys
 import usb.core
 # find USB devices
 dev = usb.core.find(find_all=True)
+
+
+#vendors = {}
+#with open('usb_vid.txt') as f:
+#    next(f)
+#    for line in f:
+#        vendors.update(f.readline().strip().split('\t'))
+#
+#vendors['0001']
+
 # loop through devices, printing vendor and product ids in decimal and hex
 for cfg in dev:
   sys.stdout.write('Decimal VendorID=' + str(cfg.idVendor) + ' & ProductID=' + str(cfg.idProduct) + '\n')
