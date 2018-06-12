c53fb67be87f2fc332ed04be5248d96a0bd8fc22
Andrei Kniazev <andrei.kniazev@yorlabs.com>
2018-06-12 07:17:08 -0700

    Starting python web app

--- megacourse/webmap/map1.py
+++ megacourse/webmap/map1.py
@@ -8,10 +8,19 @@ lat = list(coords["LAT"])
 lon = list(coords["LON"])
 elev = list(coords["ELEV"])
 
+def color_picker(elevation):
+    if elevation < 2000:
+        return 'green'
+    elif elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
 #map.add_child(folium.Marker(location=[45.43, -122.54], popup="Hi I am a Marker",icon=folium.Icon(color='green')))
 fg = folium.FeatureGroup(name="My Map")
 for lt,ln,el in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el) + 'm',icon=folium.Icon(color='green')))
+    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el) + 'm',
+    icon=folium.Icon(color=color_picker(el))))
 map.add_child(fg)
 
 
